72fd78af1a159c6a407396aa4f1ef3dc233b0947
PRB18 <baburishi505@gmail.com>
2026-02-23 15:30:40 +0530

    initial commit

---
+++ example.jsx
@@ -0,0 +1,8 @@
+function skill_input() {
+    return (
+        <div>
+            <input type="input" placeholder="Write the skill" />
+            <button>Add Skill</button>
+        </div>
+    )
+}
