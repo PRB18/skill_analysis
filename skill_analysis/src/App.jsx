4d0560fec37ee5a00ca534f5de5f3e079406d419
PRB18 <baburishi505@gmail.com>
2026-02-27 00:05:23 +0530

    conditional rendering and list rendering added

--- skill_analysis/src/App.jsx
+++ skill_analysis/src/App.jsx
@@ -3,12 +3,34 @@ import OpportunityCard from "./OpportunityCard.jsx"
 
 
 function App() {
+
+  /*const missingSkills = {
+    id:1,
+    role: "backend dev",
+    company: "rajsheker factory",
+    mskills: []
+  }
+  const companyList = [{
+    id:1,
+    role: "Web Dev",
+    company: "infosys",
+    rskills: ["React", "MongoDB", "Node"],
+    oskills: ["python", "devops"]
+  },
+  {
+    id:2,
+    role: "backend dev",
+    company: "cognizant",
+    rskills: ["python", "ml"],
+    oskills: ["css", "docker"]
+  }
+
+  ]*/
   return (
     <>
-      <OpportunityCard role="Web Dev" company="infosys" rskills="React,MongoDB,Node" oskills="python,devops"/>
-      <OpportunityCard role="backend dev" company="cognizant" rskills="python,ml" oskills="css,docker" />
-      <OpportunityCard />
-      <SkillInput/>
+      <OpportunityCard id={1} role="backend dev" company="infosis" rskills={["python","git","flask","react","api"]} 
+                        oskills={["word","excel"]} skills={[]}/>
+      <SkillInput />
     </>
   )
 
--- skill_analysis/src/OpportunityCard.jsx
+++ skill_analysis/src/OpportunityCard.jsx
@@ -1,27 +1,41 @@
-import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types'
 
-function OpportunityCard({ 
-    role = "None", 
-    company = "None", 
-    rskills = "None", 
-    oskills = "None",
-    ...props 
-}) {
-    return(
-        <div className="OpportunityCard" {...props}>
-            <h1>{role}</h1>
-            <h3>{company}</h3>
-            <p>{rskills}</p>
-            <p>{oskills}</p>
-        </div>
+function OpportunityCard(props) {
+    return (
+        <>
+            <div>
+                <h4>{props.id}</h4>
+                <h1>{props.role}</h1>
+                <h2>{props.company}</h2>
+                <h3>Required SKills:</h3>
+                <h3><ol>
+                    {props.rskills.map(
+                        rskill => <li key={props.rskills}>
+                            {rskill}
+                        </li>
+                    )}
+                </ol></h3>
+                <h3>Optional Skills</h3>
+                <h3><ol>
+                    {props.oskills.map(
+                        oskill => <li key={props.oskills}>
+                            {oskill}
+                        </li>
+                    )}
+                </ol></h3>
+                <h3>Skills having:</h3>
+                <h3><ol>
+                    {props.skills.map(
+                        skill => <li key={props.skills}>
+                            {skill}
+                        </li>
+                    )}
+                    </ol></h3>
+                {props.skills.length === 0 ? <h1>100% match</h1> : <h1>doesnt fully match</h1>}
+            </div>
+        </>
     )
 }
 
-OpportunityCard.propTypes = {
-    role: PropTypes.string,
-    company: PropTypes.string,
-    rskills: PropTypes.string,
-    oskills: PropTypes.string
-};
 
 export default OpportunityCard
