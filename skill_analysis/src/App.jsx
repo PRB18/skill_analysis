22c70906bca2f3b7526179ca1d37da14e5ef8772
PRB18 <baburishi505@gmail.com>
2026-02-27 01:11:45 +0530

    architectural changes

--- skill_analysis/src/App.jsx
+++ skill_analysis/src/App.jsx
@@ -4,35 +4,40 @@ import OpportunityCard from "./OpportunityCard.jsx"
 
 function App() {
 
-  /*const missingSkills = {
-    id:1,
-    role: "backend dev",
-    company: "rajsheker factory",
-    mskills: []
+  const missingSkills = {
+    //id:1,
+    //role: "backend dev",
+    mskills: ["React", "MongoDB", "Node"]
   }
   const companyList = [{
-    id:1,
+    id: 1,
     role: "Web Dev",
     company: "infosys",
     rskills: ["React", "MongoDB", "Node"],
     oskills: ["python", "devops"]
   },
   {
-    id:2,
+    id: 2,
     role: "backend dev",
     company: "cognizant",
     rskills: ["python", "ml"],
     oskills: ["css", "docker"]
   }
 
-  ]*/
+  ]
   return (
     <>
-      <OpportunityCard id={1} role="backend dev" company="infosis" rskills={["python","git","flask","react","api"]} 
-                        oskills={["word","excel"]} skills={[]}/>
-      <SkillInput />
+      {companyList.map(job => (
+        <OpportunityCard
+          key={job.id}
+          role={job.role}
+          company={job.company}
+          rskills={job.rskills}
+          oskills={job.oskills}
+          userskills={missingSkills.mskills}
+        />
+      ))}
     </>
   )
-
 }
 export default App
--- skill_analysis/src/OpportunityCard.jsx
+++ skill_analysis/src/OpportunityCard.jsx
@@ -1,40 +1,16 @@
 //import PropTypes from 'prop-types'
 
 function OpportunityCard(props) {
-    return (
-        <>
+
+        return (
             <div>
-                <h4>{props.id}</h4>
-                <h1>{props.role}</h1>
-                <h2>{props.company}</h2>
-                <h3>Required SKills:</h3>
-                <h3><ol>
-                    {props.rskills.map(
-                        rskill => <li key={props.rskills}>
-                            {rskill}
-                        </li>
-                    )}
-                </ol></h3>
-                <h3>Optional Skills</h3>
-                <h3><ol>
-                    {props.oskills.map(
-                        oskill => <li key={props.oskills}>
-                            {oskill}
-                        </li>
-                    )}
-                </ol></h3>
-                <h3>Skills having:</h3>
-                <h3><ol>
-                    {props.skills.map(
-                        skill => <li key={props.skills}>
-                            {skill}
-                        </li>
-                    )}
-                    </ol></h3>
-                {props.skills.length === 0 ? <h1>100% match</h1> : <h1>doesnt fully match</h1>}
+                {props.role} <br /> {props.company} <br /> <b>{props.rskills}</b> <br /> {props.oskills} <br />
+                {props.userskills.length === props.rskills.length &&
+                    props.rskills.every(item => props.userskills.includes(item)) ? <h3>100% match!</h3>
+                    : <h3>doesnt entirely match!</h3>}
             </div>
-        </>
-    )
+        )
+    
 }
 
 
